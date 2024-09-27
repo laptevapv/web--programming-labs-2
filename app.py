7b678a75faf5fb6308a8585c3efd6111a6ae5a9d
Лаптева Полина <polinalap2204@gmail.com>
2024-09-27 14:36:57 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,23 +1,74 @@
 
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 @app.route('/')
+@app.route('/index')
 def start():
-    return """
+    return redirect ("/menu", code=302)
+@app.route('/menu')
+def menu():
+    return '''
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Лаптева Полина Владимировна, лабораторная 1</title>        
+        <title>НГТУ, ФБ, Лабораторные работы</title>   
     </head>
     <body>
         <header>
-            НГТУ, ФБ, Лабораторная работа 1 
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
         </header>
-         <h1> web - сервер на flask </h1>
-         <footer>
-         &copy; Лаптева Полина, ФБИ-24, 3 курс, 2024
-         </footer>
-     </body>
-</html>
+        <h2><a href="/lab1">Первая лабораторная</a></h2>
+        <h2><a href="/lab2">Вторая лабораторная</a></h2>
+        <h2><a href="/lab3">Третья лабораторная</a></h2>
+        <h2><a href="/lab4">Четвёртая лабораторная</a></h2>
+        <h2><a href="/lab5">Пятая лабораторная</a></h2>
+        <h2><a href="/lab6">Шестая лабораторная</a></h2>
+        <h2><a href="/lab7">Седьмая лабораторная</a></h2>
+        <h2><a href="/lab8">Восьмая лабораторная</a></h2>
+        <h2><a href="/lab9">Девятая лабораторная</a></h2>
 
- """
+        <footer>
+            &copy; Лаптева Полина, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>'''
+
+@app.route('/lab1')
+def lab1():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>Лаптева Полина Владимировна, Лабораторная 1</title>
+        <style>
+            body {
+                font-family: 'Arial', sans-serif;
+                background-color: lightpurple;
+                color: #333;
+                line-height: 1.6;
+                margin: 20px;
+                font-size: 15px;
+            }
+            h1 {
+                text-align: center;
+                color: blue;
+                margin-bottom: 10px;
+        
+        </style>
+    </head>
+
+    <body>
+        <header>
+            Лабораторная работа 1
+        </header>
+
+        <h1>
+            Flask — фреймворк для создания веб-приложений на языке
+            программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+            называемых микрофреймворков — минималистичных каркасов
+            веб-приложений, сознательно предоставляющих лишь самые базовые возможности. <br>        
+            
+            <a href="/">Меню</a>
+        </h1> 
+        '''
